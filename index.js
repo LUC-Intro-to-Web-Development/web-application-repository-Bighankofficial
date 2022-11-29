846930d810c0bb350a7619c3f253fce75b071d3a
Christopher Davis <crdavis0710@gmail.com>
2022-11-28 20:50:08 -0600

    Chris Davis proving he can commit #2

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function square(n) {
   }
   
   console.log(square(5)); // 25
+
+function myFunction(a, b) {
+  return a * b;  // Function returns the product of a and b
+}
+
+  console.log(myFunction(4, 3));   // Function is called, return value will end up in x)
